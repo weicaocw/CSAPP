0a9525071e07718299a5d2b4596304fa5de452c0
caowei <weicao92@163.com>
2023-04-10 18:19:04 +0800

    fxi

--- terraform/runner/aws/main.tf
+++ terraform/runner/aws/main.tf
@@ -118,7 +118,7 @@ resource "aws_spot_fleet_request" "fleet_request" {
         ./config.sh --url ${var.github_endpoint} \
           --token \$TOKEN \
           --disableupdate \
-          --label ${var.platform}
+          --label ${var.platform} \
           --unattended > ~/config.log 2>&1
         nohup ./run.sh > ~/runner.log 2>&1
         EOS
